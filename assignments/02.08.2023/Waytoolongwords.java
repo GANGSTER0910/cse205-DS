7f97c89bc905dc1b5fc44bd74ea1149ba9d470e5
HarshPanchal <harsh.p4@ahduni.edu.in>
2023-08-16 13:38:44 +0530

    9-8-2023

---
+++ assignments/02.08.2023/Waytoolongwords.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Waytoolongwords {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        String n = input.nextLine();
+        int j = 0;
+        if(n.length()<=10){
+            System.out.println(n);
+        }else{ 
+                System.out.print(n.charAt(0));
+                System.out.print(n.length()-2);
+                System.out.print(n.charAt(n.length()-1));
+                    }
+                }
+            }
+        
+    
