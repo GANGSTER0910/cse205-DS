b532d1335447264e2698ed6a6f28180a9622b40a
HarshPanchal <harsh.p4@ahduni.edu.in>
2023-08-23 12:55:44 +0530

    23-8-23

---
+++ assignments/02.08.2023/39.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        getList(0, candidates, target, result, new ArrayList<Integer>());
+
+        return result;
+    }
+
+    public void getList(int i, int[] candidates, int target, List<List<Integer>> result, ArrayList<Integer> list)
+    {
+        if(target == 0)
+        {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        if(i == candidates.length)
+            return;
+
+        
+        int sub = target - candidates[i];
+        if(sub >= 0)
+        {
+            list.add(candidates[i]);
+            getList(i, candidates, target - candidates[i], result, list);
+            list.remove(list.size() - 1);
+        }
+        getList(i+1, candidates, target, result, list);
+        
+        }
+}
